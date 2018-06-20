353ec155292c5c0c4045c24aa3c61f7d907e27b5
Dennis McNamara <Dennis.McNamara@j2global.com>
2018-06-20 15:47:49 -0400

    update image component and traits for mjml plugin

--- src/utils/Droppable.js
+++ src/utils/Droppable.js
@@ -80,17 +80,6 @@ export default class Droppable {
           model.set('activeOnRender', 0);
         }
 
-        //add special handling for image components wrapped in customImg types
-      //   if(model) {
-      //     if (model.get('type') === "customImg") {
-      //       //set the child img element to active
-      //       var imgComp = model.get('components');
-      //       if(imgComp) {
-      //         imgComp.models[0].trigger('active');
-      //         imgComp.models[0].set('activeOnRender', 0);
-      //       }
-      //     }
-      //  }
 
         model && em.trigger('canvas:drop', dt, model);
       },
